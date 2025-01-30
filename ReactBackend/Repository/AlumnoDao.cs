1f54c519abaa78ec668e310e33b35f6801e3ad4a
AndreaN <nm0753032023@unab.edu.sv>
2025-01-29 23:48:03 -0600

    Guia 3: Inicio del CRUD

---
+++ ReactBackend/Repository/AlumnoDao.cs
@@ -0,0 +1,33 @@
+﻿using ReactBackend.Context;
+using ReactBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactBackend.Repository
+{
+    public class AlumnoDao
+    {
+        #region Contex
+        public RegistroAlumnoContext contexto = new RegistroAlumnoContext();
+        #endregion
+
+        #region Select All
+        public List<Alumno> SelectAll()
+        {
+            var alumno = contexto.Alumnos.ToList<Alumno>();
+            return alumno;
+        }
+        #endregion
+
+        #region Seleccionamos por ID
+        public Alumno? GetById(int id)
+        {
+            var alumno = contexto.Alumnos.Where(x => x.Id == id).FirstOrDefault();
+            return alumno == null ? null : alumno;
+        }
+        #endregion
+    }
+}
---
+++ Temporal/Program.cs
@@ -0,0 +1,16 @@
+﻿using ReactBackend.Repository;
+
+AlumnoDao alumnoDao = new AlumnoDao();
+
+#region SelectAll
+var alumno = alumnoDao.SelectAll();
+foreach (var item in alumno)
+{
+    Console.WriteLine(item.Nombre);
+}
+#endregion
+Console.WriteLine(" ");
+#region SlectById
+var selectById = alumnoDao.GetById(1000);
+Console.WriteLine(selectById?.Nombre);
+#endregion
