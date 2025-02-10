ae5ad0b1c5344dd225e83238ae04c73c9e7042c9
AndreaN <nm0753032023@unab.edu.sv>
2025-02-09 23:11:41 -0600

    Guia 8: Listado de alumnos de un profesor

--- ReactBackend/Repository/AlumnoDao.cs
+++ ReactBackend/Repository/AlumnoDao.cs
@@ -110,7 +110,7 @@ namespace ReactBackend.Repository
         }
         #endregion
 
-        #region
+        #region leftjoin
         public List<AlumnoAsignatura> SelectAlumAsig()
         {
             var consulta = from a in contexto.Alumnos
@@ -122,6 +122,28 @@ namespace ReactBackend.Repository
                                NombreAsignatura = asig.Nombre
                            };
             return consulta.ToList();
+        }
+        #endregion
+
+        #region leftJoinAlumnoMatriculaMateria
+        public List<AlumnoProfesor> alumnoProfesor(string nombreProfesor)
+        {
+            var listadoALumno = from a in contexto.Alumnos
+                                join m in contexto.Matriculas on a.Id equals m.AlumnoId
+                                join asig in contexto.Asignaturas on m.AsignaturaId equals asig.Id
+                                where asig.Profesor == nombreProfesor
+                                select new AlumnoProfesor
+                                {
+                                    Id = a.Id,
+                                    Dni = a.Dni,
+                                    Nombre = a.Nombre,
+                                    Direccion = a.Direccion,
+                                    Edad = a.Edad,
+                                    Email = a.Email,
+                                    Asignatura = asig.Nombre
+                                };
+
+            return listadoALumno.ToList();
         }
         #endregion
     }
---
+++ WebApi/Controllers/AlumnoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReactBackend.Models;
+using ReactBackend.Repository;
+
+namespace WebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AlumnoController : ControllerBase
+    {
+        private AlumnoDao _dao = new AlumnoDao();
+
+        #region endPointAlumnoProfesor
+        [HttpGet("alumnoProfesor")]
+        public List<AlumnoProfesor> GetAlumnoProfesor(string usuario)
+        {
+            return _dao.alumnoProfesor(usuario);
+        }
+        #endregion
+    }
+}
