c24947a4dfe6c85e1ae7cef0c800ad70f31963fc
AndreaN <nm0753032023@unab.edu.sv>
2025-02-09 22:48:52 -0600

    Guia 7: Endpoint para login

---
+++ ReactBackend/Repository/ProfesorDao.cs
@@ -0,0 +1,26 @@
+﻿using ReactBackend.Context;
+using ReactBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactBackend.Repository
+{
+    public class ProfesorDao
+    {
+        #region context
+        public RegistroAlumnoContext context = new RegistroAlumnoContext();
+        #endregion
+
+        #region GetById
+        public Profesor login(string usuario, string pass)
+        {
+            var prof = context.Profesors.Where
+                (p => p.Usuario == usuario && p.Pass == pass).FirstOrDefault();
+            return prof;
+        }
+        #endregion
+    }
+}
---
+++ WebApi/Controllers/ProfesorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReactBackend.Models;
+using ReactBackend.Repository;
+
+namespace WebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ProfesorController : ControllerBase
+    {
+        public ProfesorDao _proDao = new ProfesorDao();
+
+        [HttpPost("autentificacion")]
+        public string loginProfesor([FromBody] Profesor prof)
+        {
+            var prof1 = _proDao.login(prof.Usuario, prof.Pass);
+            if (prof1 != null)
+            {
+                return prof1.Usuario;
+            }
+            return "Elemento no encontrado";
+        }
+    }
+}
