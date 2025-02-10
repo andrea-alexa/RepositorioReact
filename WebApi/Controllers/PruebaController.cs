5a157982edccd5e0e2830b030e7c3db699b24667
AndreaN <nm0753032023@unab.edu.sv>
2025-02-09 20:22:09 -0600

    Guia 6: Introducción ASP.net

---
+++ WebApi/Controllers/PruebaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PruebaController : ControllerBase
+    {
+        #region
+        [HttpGet("prueba")]
+        public string Get()
+        {
+            return "Hola mundo";
+        }
+        #endregion
+    }
+}
