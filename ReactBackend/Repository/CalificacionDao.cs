2caa986aae457f10fa96cdc7a19ae3823276532f
AndreaN <nm0753032023@unab.edu.sv>
2025-03-01 00:38:32 -0600

    Guia 12: EndPoint para calificaciones

---
+++ ReactBackend/Repository/CalificacionDao.cs
@@ -0,0 +1,41 @@
+﻿using ReactBackend.Context;
+using ReactBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactBackend.Repository
+{
+    public class CalificacionDao
+    {
+        private RegistroAlumnoContext _contexto = new RegistroAlumnoContext();
+
+        #region Seleccionar_lista_calificaciones
+        public List<Calificacion> seleccion(int matriculaid)
+        {
+            var matricula = _contexto.Matriculas.Where(x => x.Id == matriculaid).ToList();
+            Console.WriteLine("matricula encontrada");
+            try
+            {
+                if (matricula != null)
+                {
+                    var calificacion = _contexto.Calificacions.Where(x => x.Id == matriculaid).ToList();
+
+                    return calificacion;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        #endregion
+    }
+}
---
+++ WebApi/Controllers/CalificacionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReactBackend.Models;
+using ReactBackend.Repository;
+
+namespace WebApi.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class CalificacionController : ControllerBase
+    {
+        private CalificacionDao _cdao = new CalificacionDao();
+
+        #region Lista de Calificaciones
+        [HttpGet("calificaciones")]
+
+        public List<Calificacion> get(int idMatricula)
+        {
+            return _cdao.seleccion(idMatricula);
+        }
+        #endregion
+    }
+}
